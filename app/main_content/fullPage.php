b7a29d56e20b7bc0f0fc808c1091f269b43dc7ed
Julia Dorneles <113448194+Jad3102@users.noreply.github.com>
2025-07-02 10:34:47 -0300

    Update fullPage.php

--- app/main_content/fullPage.php
+++ app/main_content/fullPage.php
@@ -14,7 +14,7 @@ $loggedIn = isset($_SESSION['usuario_id']);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel = "stylesheet" type="text/css" href="../assets/style.css">
+    <link rel = "stylesheet" type="text/css" href="app/assets/style.css">
     </head>
 
   <body>
